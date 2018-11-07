6c32e3c3c39f91968ccf1b353d991efd54663aba
Lucas Nogueira <lucasnogueiratdm@hotmail.com>
2018-11-07 15:22:22 -0200

    0.0.7

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,15 @@
-var $target = ($)
+function inicializar(){
+    document.getElementById("Sobre").className    += "esconder";
+    document.getElementById("Cardapio").className += "esconder";
+    document.getElementById("Quests").className   += "esconder";
+    document.getElementById("Contato").className  += "esconder";
+}
+
+function descendo(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        document.getElementById("Sobre").className = "sobre animated fadeInLeft"; 
+    }
+}
+
+window.onscroll = function() {descendo()};
+
